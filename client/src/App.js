f4c914e277610303a0ded4885d303a04be2bd948
Shubham Vishwakarma <shubham.vishwakarma@justdial.com>
2020-07-11 03:38:13 +0530

    Changes

--- client/src/App.js
+++ client/src/App.js
@@ -13,17 +13,6 @@ function App() {
     <BrowserRouter>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
       <Link to='/otherpage' children='Other' />
       <Link to='/' children='Home' />
